c56a319026f9bf4b9ee4207a6195f33886cdb0c0
nu11p01n73R <kjjohny1991@gmail.com>
2016-11-29 08:36:39 +0530

    connection.Connect to connect to sql database

--- mysql_migrate.go
+++ mysql_migrate.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"database/sql"
+	"errors"
 	"flag"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"golang.org/x/crypto/ssh/terminal"
+	"os"
+	"syscall"
 )
 
 // Type connection defines a connection
@@ -10,7 +16,29 @@ import (
 type connection struct {
 	Host     string
 	Username string
+	Dbname   string
 	Password string
+	Db       *sql.DB
+}
+
+// Connects to the database with the available
+// credentials on conn
+// returns
+// 	-sql.DB on successsfull connection
+//  -error on any error occured while connecting to db.
+func (conn connection) Connect() (*sql.DB, error) {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s", conn.Username, conn.Password, conn.Host, conn.Dbname)
+	db, err := sql.Open("mysql", dsn)
+
+	if err != nil {
+		return nil, errors.New("Unable to open mysql connection")
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, errors.New("Database ping failed with error : " + err.Error())
+	}
+	return db, nil
 }
 
 // Parses command line flags
@@ -19,14 +47,26 @@ func parseConnectionFlags() *connection {
 	// Available flags.
 	host := flag.String("h", "", "MySQL host url")
 	username := flag.String("u", "", "MySQL user name")
-	password := flag.String("p", "", "MySQL password")
+	dbname := flag.String("db", "", "MySQL database name")
 
 	flag.Parse()
 
-	return &connection{Host: *host, Username: *username, Password: *password}
+	var password string
+	fmt.Printf("Enter password for %s@%s\n", *username, *host)
+	bytePasssd, _ := terminal.ReadPassword(int(syscall.Stdin))
+	password = string(bytePasssd)
+
+	return &connection{Host: *host, Username: *username, Password: password, Dbname: *dbname}
 }
 
 func main() {
 	connection := parseConnectionFlags()
-	fmt.Println(connection)
+	_, err := connection.Connect()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Println("Successfully connected")
 }
